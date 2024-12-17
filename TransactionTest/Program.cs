8025e7422a542c49584b661c5b2f3b1c1e080a82
levon@sestive.com <levon@sestive.com>
2024-12-17 20:17:29 +0400

    Add project files.

---
+++ TransactionTest/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace TransactionTest
+{
+    internal class Program
+    {
+        private const string ConnectionString = "Data Source=lectures-db-dev.database.windows.net;Initial Catalog=BlocknotDB;Persist Security Info=True;User ID=ad;Password=Ararat73;Encrypt=True;Trust Server Certificate=True";
+
+        static void Main(string[] args)
+        {
+            // indert numbers from 1 to 100 to Numbers table of the DB
+
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                SqlTransaction transaction = connection.BeginTransaction();
+
+                string commandText = "INSERT INTO Numbers (Number) VALUES (@Value)";
+
+                SqlCommand command = new SqlCommand(commandText, connection);
+
+                command.Transaction = transaction;
+
+                SqlParameter parameter = command.CreateParameter();
+                parameter.ParameterName = "@Value";
+                command.Parameters.Add(parameter);
+
+                try
+                {
+                    for (int i = 1; i <= 100; i++)
+                    {
+                        //if (i == 50)
+                        //{
+                        //    throw new Exception("Something went wrong");
+                        //}
+                        parameter.Value = i;
+                        command.ExecuteNonQuery();
+                    }
+
+                    transaction.Commit();
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                }
+            }
+        }
+    }
+}
